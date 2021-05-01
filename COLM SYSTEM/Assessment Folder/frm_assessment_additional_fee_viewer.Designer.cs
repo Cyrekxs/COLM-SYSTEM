6c7e72b77e852f3c4874a42eefbf7de6568d3fa7
Cyrekxs <anthonyquijano20@gmail.com>
2021-05-01 10:02:01 +0800

    Assessment modification -revised schedule ds and schedule model -remove additional fee in tuition fee entry -revised subject setted and fee

---
+++ COLM SYSTEM/Assessment Folder/frm_assessment_additional_fee_viewer.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace COLM_SYSTEM.Assessment_Folder
+{
+    partial class frm_assessment_additional_fee_viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clmAdditionalFeeID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmAdditionalFeeAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdditionalFees = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmAdditionalFeeID,
+            this.Column1,
+            this.clmAdditionalFeeAmount,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 64);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(544, 196);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // clmAdditionalFeeID
+            // 
+            this.clmAdditionalFeeID.HeaderText = "Additional Fee ID";
+            this.clmAdditionalFeeID.Name = "clmAdditionalFeeID";
+            this.clmAdditionalFeeID.ReadOnly = true;
+            this.clmAdditionalFeeID.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Fee Description";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // clmAdditionalFeeAmount
+            // 
+            this.clmAdditionalFeeAmount.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmAdditionalFeeAmount.HeaderText = "Amount";
+            this.clmAdditionalFeeAmount.Name = "clmAdditionalFeeAmount";
+            this.clmAdditionalFeeAmount.ReadOnly = true;
+            this.clmAdditionalFeeAmount.Width = 68;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Add\'l Type";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 82;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Additional Fees";
+            // 
+            // txtAdditionalFees
+            // 
+            this.txtAdditionalFees.Location = new System.Drawing.Point(448, 266);
+            this.txtAdditionalFees.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAdditionalFees.Name = "txtAdditionalFees";
+            this.txtAdditionalFees.ReadOnly = true;
+            this.txtAdditionalFees.Size = new System.Drawing.Size(111, 25);
+            this.txtAdditionalFees.TabIndex = 2;
+            this.txtAdditionalFees.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SUBJECT";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(15, 30);
+            this.txtSubject.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.ReadOnly = true;
+            this.txtSubject.Size = new System.Drawing.Size(543, 25);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // frm_assessment_additional_fee_viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 312);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdditionalFees);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_assessment_additional_fee_viewer";
+            this.Text = "LIST OF ADDITIONAL FEE";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmAdditionalFeeID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmAdditionalFeeAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdditionalFees;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubject;
+    }
+}
---
+++ COLM SYSTEM/Assessment Folder/frm_assessment_additional_fee_viewer.cs
@@ -0,0 +1,30 @@
+﻿using COLM_SYSTEM_LIBRARY.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COLM_SYSTEM.Assessment_Folder
+{
+    public partial class frm_assessment_additional_fee_viewer : Form
+    {
+        public frm_assessment_additional_fee_viewer(string subject, List<SubjectSettedAddtionalFee> additionalFees)
+        {
+            InitializeComponent();
+            txtSubject.Text = subject;
+            double totalAdditionalFee = 0;
+            foreach (var item in additionalFees)
+            {
+                dataGridView1.Rows.Add(item.AdditionalFeeID, item.FeeDescription, item.Amount.ToString("n"), item.FeeType);
+                totalAdditionalFee += item.Amount;
+            }
+
+            txtAdditionalFees.Text = totalAdditionalFee.ToString("n");
+        }
+    }
+}
