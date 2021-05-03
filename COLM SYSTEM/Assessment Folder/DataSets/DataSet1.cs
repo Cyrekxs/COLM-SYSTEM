1408140f3cfd33bbb5fe5826d3226e230c630524
Anthony Quijano <anthonyquijano20@gmail.com>
2021-05-03 17:01:21 +0800

    assessment revision

---
+++ COLM SYSTEM/Assessment Folder/DataSets/DataSet1.cs
@@ -0,0 +1,18 @@
+﻿namespace COLM_SYSTEM.Assessment_Folder.DataSets
+{
+}
+
+namespace COLM_SYSTEM.Assessment_Folder.DataSets
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
+namespace COLM_SYSTEM.Assessment_Folder.DataSets {
+    
+    
+    public partial class DataSet1 {
+    }
+}
