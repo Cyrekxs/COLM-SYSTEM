117650607fb4949001bdca0ddeba8e5a3518f14a
Cyrekxs <anthonyquijano20@gmail.com>
2021-05-18 03:32:40 +0800

    removed frm_payment_entry add frm_payment_cash_entry

---
+++ COLM SYSTEM/Payment Folder/frm_payment_cash_entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COLM_SYSTEM.Payment_Folder
+{
+    public partial class frm_payment_cash_entry : Form
+    {
+        public frm_payment_cash_entry(double AmountToPay)
+        {
+            InitializeComponent();
+            txtAmount.Text = AmountToPay.ToString("n");
+        }
+    }
+}
