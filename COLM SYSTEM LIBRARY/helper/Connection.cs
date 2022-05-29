b84a2f4696ac30811399edd0c377470b89a1e74d
Cyrekxs <anthonyquijano20@gmail.com>
2022-05-29 22:53:45 +0800

    -remove signing in project properties for transferring into stda education

--- COLM SYSTEM LIBRARY/helper/Connection.cs
+++ COLM SYSTEM LIBRARY/helper/Connection.cs
@@ -6,7 +6,7 @@
 
         //////local connection SYSTEM   
         //Online Connection
-        public static string LStringConnection { get { return @"Data Source=162.241.240.48;Initial Catalog=colmpulilan_server;User Id=colmpulilan_sysadmin;Password=Admin.c0lm2o18;"; } }
+        //public static string LStringConnection { get { return @"Data Source=162.241.240.48;Initial Catalog=colmpulilan_server;User Id=colmpulilan_sysadmin;Password=Admin.c0lm2o18;"; } }
 
         //My School Desktop Connection
         //public static string LStringConnection { get { return @"Data Source=COLM\SQLEXPRESS01;Initial Catalog=colmpulilan_server;Integrated Security=True"; } }
@@ -25,22 +25,11 @@
 
         #region connections
         /// <summary>
-        /// COLM SYSTEM
-        /// </summary>
-
-        //////local connection COLM
-        ////public static string LStringConnection { get { return @"Server=hgws12.win.hostgator.com;Database=colmpulilan_server;User Id=colmpulilan_sysadmin;Password=Admin.c0lm2o18"; } }
-        //public static string LStringConnection { get { return @"Data Source=COLM\SQLEXPRESS;Initial Catalog=colmpulilan_server;Persist Security Info=True;User ID=sa;Password=sa"; } }
-        //////online connection COLM
-        //public static string OStringConnection { get { return @"Server=hgws12.win.hostgator.com;Database=colmpulilan_server_registration;User Id=colmpulilan_sysadmin;Password=Admin.c0lm2o18"; } }
-
-
-        /// <summary>
         /// STDA SYSTEM
         /// </summary>
 
         ////local connection STDA //ns51.win.hostgator.com
-        //public static string LStringConnection { get { return @"Server=ns51.win.hostgator.com;Database=stda_server;User Id=stda_sysadmin;Password=stda.admin2o21"; } }
+        public static string LStringConnection { get { return @"Data Source=PCCYREKXS\SQLEXPRESS;Initial Catalog=stda_server_updated;Integrated Security=True"; } }
         ////online connection STDA
         //public static string OStringConnection { get { return @"Server=ns51.win.hostgator.com;Database=stda_server_registration;User Id=stda_sysadmin;Password=stda.admin2o21;"; } }
         #endregion
